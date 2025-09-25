056c83c0d1b61b224c92e2ac5f951f5cee6e6d23
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-25 15:40:29 -0300

    feat: Garantindo estilo de código no Node js com prettier

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
 import { Server } from '@overnightjs/core';
-import './utils/mudule-alias'
+import './utils/mudule-alias';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
   constructor(private port = 8080) {
-    super()
+    super();
   }
 
   public init(): void {
-    this.setupExpress()
-    this.setupControllers()
+    this.setupExpress();
+    this.setupControllers();
   }
 
   private setupExpress(): void {
-    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.json());
   }
 
   private setupControllers(): void {
-    const forecasController = new ForecastController()
-    this.addControllers([forecasController])
+    const forecasController = new ForecastController();
+    this.addControllers([forecasController]);
   }
 
   public getApp(): Application {
-    return  this.app;
+    return this.app;
   }
 }
