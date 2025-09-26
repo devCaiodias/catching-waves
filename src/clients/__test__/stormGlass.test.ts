7463a7b5da51a8b337659ebc9bc5ae78a3d7e80a
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-25 22:43:48 -0300

    feat: Criando o API client para consumir a API externa de meteorologia

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixture/stormGlass_weather_3_hours.json'
+import stomGlassWeatherNormalized3HoursFixture from '@test/fixture/stormGlass_normalized_response_3_hours.json'
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.featshPoints(lat, lng)
+    expect(response).toEqual(stomGlassWeatherNormalized3HoursFixture)
+  })
+})
